51065492396cb51df7e2a72a44f047a4aaf57ee3
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-08-02 17:28:16 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors")
 
 const app = express()
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors())
 
@@ -21,4 +21,4 @@ app.get('/compoundinterest', function(req, res){
 })
 
 
-app.listen(port)
+app.listen(PORT)
