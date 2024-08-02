c4c8da83f1a325d7dacc4589a16de92875d099b0
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-08-02 16:32:13 +0530

    formula added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors")
+
+const app = express()
+const port = 8080;
+
+app.use(cors())
+
+app.get('/compoundinterest', function(req, res){
+    const principal = parseInt(req.query.principal);
+    const rate = parseInt(req.query.rate);
+    const time = parseInt(req.query.time);
+
+    const amount = principal * Math.pow((1 + rate / 100), time)
+    const ci = amount - principal;
+
+    res.json({
+        amount: amount,
+        compoundInterest: ci
+    })
+})
+
+
+app.listen(port)
